f5d13b64675be47caa7b3ea2b4df084054cd59f1
Gabriel Huguenin-Dumittan <gabriel.huguenin-dumittan@epitech.eu>
2021-11-02 16:42:20 +0100

    App index

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+import {decode, encode} from 'base-64'
+
+if (!global.btoa) {  global.btoa = encode }
+
+if (!global.atob) { global.atob = decode }
+
+AppRegistry.registerComponent(appName, () => App);
